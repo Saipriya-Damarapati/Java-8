c8b7e28514713a62cb745411e8d803ca8ad27882
Sai Priya <Sai Priya@VENJYOSAI>
2020-08-24 08:45:23 +0530

    added comments in tutorial 1

--- Java-8/src/com/cglearning/tutorial_1/HelloWorldProgram.java
+++ Java-8/src/com/cglearning/tutorial_1/HelloWorldProgram.java
@@ -2,6 +2,7 @@ package com.cglearning.tutorial_1;
 
 public class HelloWorldProgram {
 
+	// main method is the starting point of the program
 	public static void main(String[] args) {
 		System.out.println("Hello World...!");
 	}
